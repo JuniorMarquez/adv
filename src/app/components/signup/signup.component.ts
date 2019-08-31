519187502f6cb3d4a4c7414ef36a4d8b2fa26487
root <root@0000>
2019-08-31 16:43:21 -0400

    login ts

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { UserInterface } from '../../models/user-interface'; 
+//import {RouterModule,Routes} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
+  private user : UserInterface ={
+  	name:"",
+  	email:"",
+  	password:""
+  };
 
   ngOnInit() {
   }
-
+  onRegister(): void{
+  	this.authService.registerUser(
+  		this.user.name, 
+  		this.user.email, 
+  		this.user.password
+  		)
+  	.subscribe(user => {
+//  		console.log(user);	
+  this.authService.setUser(user);
+   let token  = user.id;
+   this.authService.setToken(token);
+   this.router.navigate(['/affiliates']);
+   	});
+  	}
 }
